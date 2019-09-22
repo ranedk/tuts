38c0bce8e04e08d70f1adb2d7a2ad54279df55d3
Devendra K. Rane <ranedk@gmail.com>
2019-09-22 21:27:25 +0530

    pandas in ml

---
+++ ml/mltuts/README.md
@@ -0,0 +1,6 @@
+# Setup
+
+Create a virtual environment for python 3.6+
+`pip install -r requirements.txt`
+
+pip install `spyder`, which we will use to do the tutorials (https://www.spyder-ide.org/)
---
+++ ml/mltuts/pandas_2.py
@@ -0,0 +1,89 @@
+import pandas as pd
+
+
+# Create a dataframe from titanic.csv dataset
+df = pd.read_csv("data/titanic.csv")
+
+# Shows details of the dataframe - columns, total rows, non-null counts
+print(df.info())
+
+# Initial rows of a dataset
+print(df.head())
+# End 4 rows of a dataset
+print(df.tail(4))
+
+# column count and row count of a dataframe
+print(df.shape)
+
+# 4th row of dataframe
+print(df.iloc[0])
+
+# 5th row, 6th col of dataframe
+print(df.iloc[5][6])
+
+# 4th to 10th rows of dataframe
+print(df.iloc[4:7])
+
+# 4th to 7th row of dataframe, with only the 3rd col
+print(df.iloc[4:7, 3])
+
+# 4th to 7th row of dataframe, with 3rd to 7th cols
+print(df.iloc[4:7, 3:7])
+
+# Rest works like list slicing
+print(df.iloc[-20:-1: 2, 3:9:2])
+
+# Instead of auto-increment indexes, you can change index to a col
+df0 = df.set_index(df['Name'])
+
+# Index based lookup, using loc
+print(df0.loc['Dooley, Mr. Patrick'])
+
+# Index is not required to be unique
+df1 = df.set_index(df['Sex'])
+
+# Index based lookup
+print(df1.loc['male'])
+
+# Conditional lookups and filter
+print(df[df['Pclass'] == 2])
+
+print(df[(df['Pclass'] == 3) & (df['Sex'] == 'male')])
+
+print(df[(df['Age'] > 43) & (df['Sex'] == 'male')])
+
+# Add a new column based on old columns
+df['Class Type'] = df['Pclass'].replace(1, 'First').replace(2, 'Second').replace(3, 'Third')
+
+df['demography'] = df[]
+
+
+# Rename columns
+df = df.rename(columns={'Sex': 'Gender'})
+
+# Functions on columns
+print(
+      "\nmean age=                ", df['Age'].mean(),
+      "\nmax age=                 ", df['Age'].max(),
+      "\ncount of non-null ages=  ", df['Age'].count(),
+      "\nsum of all ages=         ", df['Age'].sum()
+)
+
+print(
+      "\nUnique values of gender        ", df['Gender'].unique(),
+      "\nUnique value counts of gender  ", df['Gender'].value_counts()
+)
+
+# Rows or Series
+row = df.loc[4]         # type(row) == pandas.core.series.Series
+
+print("null columns: ", row.isnull())
+
+print("non null columns: ", row.notnull())
+
+# One can do a lot of operations on Series too. row.count(), row.sum() etc.
+
+print("All rows with null in Ages  = ",  df[df['Age'].isnull()])
+
+# Dropping data
+
