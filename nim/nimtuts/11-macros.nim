3804f6ad3548e32b5372acd10a53cf3931a349ab
Devendra Rane <ranedk@gmail.com>
2021-04-12 21:03:48 +0530

    adding macros videos from David

---
+++ nim/nimtuts/11-macros.nim
@@ -0,0 +1,13 @@
+# The absolute awesomeness of Nim
+
+Of all the reasons to love Nim, the amazing DSL creation capabilities using macro programming tops my list. Macro essentially uses the AST to create safe code.
+
+The best way to learn this is David's youtube channel:
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/97egpJPhPWs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/97egpJPhPWs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
